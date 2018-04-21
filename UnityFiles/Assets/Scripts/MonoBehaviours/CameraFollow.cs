28eb4f76be578ce5a31cde76f384a313d2166dd3
AaronDSmit <smitaaron1@gmail.com>
2018-04-21 17:32:03 +1000

    Added Camera Follow Script
    Added Camera Follow Script, player moved to click point

---
+++ UnityFiles/Assets/Scripts/MonoBehaviours/CameraFollow.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    [SerializeField]
+    private Transform target;
+
+    private void Awake()
+    {
+        if (!target)
+        {
+            target = GameObject.FindGameObjectWithTag("Player").transform;
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        transform.position = new Vector3(target.position.x, transform.position.y, target.position.z);
+    }
+}
