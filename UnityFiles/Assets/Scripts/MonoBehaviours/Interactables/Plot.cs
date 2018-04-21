0623f2fa0d8e9d9928fb6e9e9afed87a77fb48f2
danjstuddert <danieljstuddert@gmail.com>
2018-04-21 21:12:07 +1000

    Interactable Class

---
+++ UnityFiles/Assets/Scripts/MonoBehaviours/Interactables/Plot.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Plot : Interactable {
+    [SerializeField] private List<Mesh> cropStages; 
+
+    private bool hasCrop;
+    private MeshFilter meshFilter;
+    private MeshRenderer meshRenderer;
+
+    private void Start () {
+            
+    }
+
+    public override void Interact () {
+
+    }
+}
