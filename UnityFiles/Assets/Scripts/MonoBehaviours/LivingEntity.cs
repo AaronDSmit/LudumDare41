a1f26655e065410ddbe302cadee8f9fb3cfe4ffa
AaronDSmit <smitaaron1@gmail.com>
2018-04-22 11:37:50 +1000

    Added Tower, player and plot script

---
+++ UnityFiles/Assets/Scripts/MonoBehaviours/LivingEntity.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LivingEntity : MonoBehaviour
+{
+    [SerializeField]
+    protected float startHealth;
+    protected float currentHealth;
+
+    protected virtual void Start()
+    {
+        currentHealth = startHealth;
+    }
+
+    public void TakeDamge(float damage)
+    {
+        currentHealth -= damage;
+
+        if (currentHealth < 0)
+        {
+            Die();
+        }
+    }
+
+    [ContextMenu("TakeDamge")]
+    public void TakeDamge()
+    {
+        currentHealth -= 1;
+
+        transform.Translate(Vector3.down);
+
+        if (currentHealth < 0)
+        {
+            Die();
+        }
+    }
+
+    protected void Die()
+    {
+
+    }
+}
---
+++ UnityFiles/Assets/Scripts/MonoBehaviours/Player.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField]
+    private Plant[] plants;
+
+    [SerializeField]
+    private int currentPlant;
+
+    private void Update()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+
+            if (Physics.Raycast(ray, out hit))
+            {
+                Plot plot = hit.transform.GetComponent<Plot>();
+
+                if (plot != null)
+                {
+                    plot.PlacePlant(plants[currentPlant]);
+                }
+            }
+        }
+    }
+}
