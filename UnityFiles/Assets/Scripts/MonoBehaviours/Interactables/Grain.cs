2a52b1a36d9732263e9f8759c70665b66053ab0e
Dan Studdert <danieljstuddert@gmail.com>
2018-04-23 13:10:19 +1000

    Started grain interactable stuff

---
+++ UnityFiles/Assets/Scripts/MonoBehaviours/Interactables/Grain.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Grain : Interactable {
+    [SerializeField] private int value;
+
+    private Tower playerTower;
+    private Tower enemyTower;
+
+    private void Awake () {
+        Tower[] towers = FindObjectsOfType<Tower>();
+
+        for (int i = 0; i < towers.Length; i++) {
+            if (towers[i].Team == Team.PLAYER)
+                playerTower = towers[i];
+            else
+                enemyTower = towers[i];
+        }
+    }
+
+    public override void Interact (Team team) {
+        if(team == Team.PLAYER) {
+           // playerTower.HealDamge(value);
+        } else {
+            // enemyTower.HealDamge(value);
+        }
+    }
+}
